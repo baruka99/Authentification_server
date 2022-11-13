c3805ddd5da5f5c468cb12033bc4287e330ad133
= <=>
2022-11-14 01:44:39 +0200

    user logged in

--- api/controller/user.js
+++ api/controller/user.js
@@ -7,116 +7,60 @@ const { randomString, sha256 } = require('../shared/utils.js');
 const jwt = require('jsonwebtoken')
 const mailSender = require('../shared/mail')
 
-exports.sAdminSignUp = (req, res) => {
+exports.sAdminSignUp = async (req, res) => {
     const email = req.body.email;
-    User.findOne(
-        { email: email }, (err, user) => {
-            if (err)
-                res.status(500).json(
-                    {
-                        message: err.message
-                    }
-                );
-            else {
-                if (user) {
-                    res.status(409).json(
-                        {
-                            message: "Une erreur s'est produite"
-                        }
-                    );
-                } else {
 
-                    let adminCode = sha256(randomString());
-                    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-                    const msg = {
-                        to: email,
-                        from: 'contact.upperz@gmail.com', //the email address or domain you verified above
-                        subject: 'Mot de passe compte admin L1000pay',
-                        text: "Voici votre mot de passe admin: " + adminCode,
-                        html: adminCode,
-                    };
+    try {
+        const user = await User.findOne({ email: email })
+        if (user) res.status(409).json({ message: "Une erreur s'est produite" });
+        else {
 
-                    // sending the code to the admin and then save the admin
-                    sgMail
-                        .send(msg)
-                        .then(
-                            (_) => {
-                                bcrypt.genSalt(
-                                    10,
-                                    function (err, salt) {
-                                        if (err) {
-                                            res.status(500).json({
-                                                message: err.message
-                                            });
-                                        } else {
-                                            bcrypt.hash(adminCode, salt, function (err, hash) {
-                                                if (err) {
-                                                    res.status(500).json({
-                                                        message: err.message
-                                                    });
-                                                } else {
-                                                    const user = User({
-                                                        _id: new mongoose.Types.ObjectId,
-                                                        email: email,
-                                                        createdAt: Date.now()
-                                                    });
-                                                    user.save()
-                                                        .then((savedUser) => {
-
-                                                            const creadential = Credential(
-                                                                {
-                                                                    _id: new mongoose.Types.ObjectId,
-                                                                    username: email,
-                                                                    password: hash,
-                                                                    user: savedUser,
-                                                                    role: "as super admin",
-                                                                    createdAt: Date.now()
-                                                                }
-                                                            );
-                                                            creadential.save()
-                                                                .then(_ => {
-                                                                    res.status(201).json({
-                                                                        message: "L'administrateur a été bien enrégistré",
-                                                                    });
-                                                                })
-
-                                                        })
-                                                        .catch(
-                                                            err => {
-                                                                res.status(500).json({
-                                                                    message: err.message
-                                                                })
-
-                                                            }
-                                                        );
-                                                }
-                                            })
-                                        }
-
-                                    }
-                                )
-                            }
-                        )
-                        .catch(
-                            err => {
-                                res.status(500).json(
-                                    {
-                                        message: err
-                                    }
-                                );
-                            }
-                        )
-
+            let adminCode = sha256(randomString());
+            sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+            const msg = {
+                to: email,
+                from: 'contact.upperz@gmail.com', //the email address or domain you verified above
+                subject: 'Mot de passe compte admin L1000pay',
+                text: "Voici votre mot de passe admin: " + adminCode,
+                html: adminCode,
+            };
+            await sgMail.send(msg)
+            const salt = await bcrypt.genSalt(10)
+            const cryptPass = await bcrypt.hash(adminCode, salt);
+            const newUser = User({
+                _id: new mongoose.Types.ObjectId,
+                email: email,
+                createdAt: Date.now()
+            });
+            await newUser.save()
+            const creadential = Credential(
+                {
+                    _id: new mongoose.Types.ObjectId,
+                    username: email,
+                    password: cryptPass,
+                    user: newUser,
+                    role: "as super admin",
+                    createdAt: Date.now()
                 }
-            }
+            );
+            await creadential.save()
+            res.status(201).json({
+                message: "L'administrateur a été bien enrégistré",
+            });
+
+
         }
-    );
+    } catch (err) {
+        res.status(500).json({
+            message: err.message
+        })
+    }
+
 }
 
 // LOGIN ADMIN
 exports.sAdminLogin = async (req, res) => {
     const { username, password } = req.body;
-
     try {
         const adminCredential = await Credential.findOne({ username }).populate('user');
 
@@ -196,7 +140,7 @@ exports.userSignUp = async (req, res) => {
                     from: 'contact.upperz@gmail.com', //the email address or domain you verified above
                     subject: 'Mot de passe compte L1000pay',
                     text: "Voici votre mot de passe: " + genPwd,
-                    html: "Voici votre mot de passe: " + genPwd,
+                    html: mailSender(genPwd),
                 };
                 await sgMail.send(msg)
             }
